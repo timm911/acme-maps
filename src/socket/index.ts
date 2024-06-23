94edd0f74ee679a4eea0d31b2e5d699924b6c094
vito-dev-10001 <royeca.dev@outlook.com>
2024-06-23 15:03:31 -0400

    Update index.ts

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 import { BACKEND_URL } from '../constants';
 
-export default io("wss://18.119.166.188/socket.io/?EIO=4&transport=websocket", {
+export default io(io, {
   transports: ["websocket"],
 });
