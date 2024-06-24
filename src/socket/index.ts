debca2fd0e80b5f126d1063b6e2faa26f458472f
vito royeca <vito.royeca@outlook.com>
2024-06-24 02:59:28 -0700

    fixed deploy issue

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 import { BACKEND_URL } from '../constants';
 
-export default io(BACKEND_URL, {
+export default io("wss://18.119.166.188/socket.io/?EIO=4&transport=websocket", {
   transports: ["websocket"],
 });
